3c869c8c2de12b68e282288eba65d98720b9f288
Nathan Cheek <myself@nathancheek.com>
2014-01-21 10:23:17 -0500

    Initial commit of BitcoinStatus

---
+++ BitcoinStatus.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+#This script gets the address balance from the blockchain.info API and multiplies it by
+#the BTC/USD exchange rate on bitcoinaverage.com
+BTCADDRESS="100000000000000000000000000000"
+pushoverNotificationsEnabled="false"
+#Fill these out if you are using Pushover notifications
+POTOKEN="000000000000000000000000000000"
+POUSER="000000000000000000000000000000"
+POSOUND="intermission"
+btcBalance=$(curl -s https://blockchain.info/rawaddr/$BTCADDRESS | grep -oP '(?<=\"final_balance\":).*' | sed 's/,.*//')
+btcBalance=$(echo "${btcBalance:0:-8}.${btcBalance: -8}")
+echo "BTC balance: $btcBalance"
+#usdPriceResult=$(curl -s https://api.bitcoinaverage.com/ticker/global/USD/)
+usdPrice=$(curl -s https://api.bitcoinaverage.com/ticker/global/USD/ | grep -oP '(?<=\"last\": ).*' | sed 's/,.*//')
+echo "Latest trade in USD: $usdPrice"
+usdValue=$(printf "%.2f\n" $(echo "$btcBalance * $usdPrice" | bc))
+echo "Value of your BTC in USD: $usdValue"
+echo "$(date +"%Y-%m-%d %T") - BTC balance: $btcBalance - Latest trade in USD: $usdPrice - Value of your BTC in USD: \$$usdValue" >> ~/BitcoinStatus.log
+if [ "$pushoverNotificationsEnabled" = "true" ]; then
+	echo "Sending notification to Pushover..."
+	curl -s -F "token=$POTOKEN" -F "user=$POUSER" -F "title=Current bitcoin value" -F "message=Balance: $btcBalance BTC - Exchange rate: \$$usdPrice - Value in USD: \$$usdValue" -F "sound=$POSOUND" https://api.pushover.net/1/messages.json > /dev/null
+fi
+echo "Done"
