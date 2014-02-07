ca987373344c2dd7cf9cacb2eb916d1bf974f171
Nathan Cheek <myself@nathancheek.com>
2014-02-07 09:51:09 -0500

    Fixed log location to same directory

--- BitcoinStatus.sh
+++ BitcoinStatus.sh
@@ -15,7 +15,7 @@ usdPriceDec=$(printf "%.2f\n" $(echo $usdPrice))
 echo "Latest trade in USD: $usdPriceDec"
 usdValue=$(printf "%.2f\n" $(echo "$btcBalance * $usdPrice" | bc))
 echo "Value of your BTC in USD: $usdValue"
-echo "$(date +"%Y-%m-%d %T") - BTC balance: $btcBalance - Latest trade in USD: $usdPrice - Value of your BTC in USD: \$$usdValue" >> ~/BitcoinStatus.log
+echo "$(date +"%Y-%m-%d %T") - BTC balance: $btcBalance - Latest trade in USD: $usdPrice - Value of your BTC in USD: \$$usdValue" >> BitcoinStatus.log
 if [ "$pushoverNotificationsEnabled" = "true" ]; then
 	echo "Sending notification to Pushover..."
 	curl -s -F "token=$POTOKEN" -F "user=$POUSER" -F "title=Current bitcoin value" -F "message=Balance: $btcBalance BTC - Exchange rate: \$$usdPrice - Value in USD: \$$usdValue" -F "sound=$POSOUND" https://api.pushover.net/1/messages.json > /dev/null
